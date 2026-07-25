df8d8ec9e8edbba0e991fd6fcecba0dad41ac31b
Kirill <kirillsidora@yandex.ru>
2026-07-25 18:13:01 +0300

    [MB-1] feat: Created Goal class

---
+++ main.py
@@ -0,0 +1,7 @@
+class Goal:
+    def __init__(self, name: str, tottal_amount: int, balance: int, category: str, status: str):
+        self.name = name
+        self.total_amount = tottal_amount
+        self.balance = balance
+        self.category = category
+        self.status = status
