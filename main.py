02f954ee889b30727de636de566d4abd65b368d5
Kirill <kirillsidora@yandex.ru>
2026-07-25 21:39:35 +0300

    [MB-5] feat: Created and implemented functions for create, delete and update goal information

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+import os
+import csv
+
 class Goal:
-    def __init__(self, name: str, tottal_amount: int, balance: int, category: str, status: str):
+    def __init__(self, name: str, tottal_amount: int, balance: int, category: str):
         self.name = name
         self.total_amount = tottal_amount
         self.balance = balance
         self.category = category
-        self.status = status
+        self.__status = 'не выполнена'
 
     def increase_balance(self, amount: int):
         try:
@@ -32,5 +35,284 @@ class Goal:
 
         return result
 
+    def get_status(self):
+        return self.__status
+
     def __change_status(self):
-        self.status = 'выполнена'
+        self.__status = 'выполнена'
+
+def create_goal():
+    while True:
+        try:
+            category = input('название категории>')
+
+            if category.strip() == '':
+                raise ValueError('ошибка>название категории не может состоять только из символов пробела и переноса на новую строку')
+
+            break
+        except ValueError as error:
+            print(error)
+
+    while True:
+        try:
+            name = input('название цели>')
+
+            if name.strip() == '':
+                raise ValueError('ошибка>название цели не может состоять только из символов пробела и переноса на новую строку')
+            elif os.path.exists(f'categoryes/{category}/{name}.csv'):
+                raise ValueError(f'ошибка>цель с названием "{name}" уже существует в категории "{category}"')
+            break
+        except ValueError as error:
+            print(error)
+
+    while True:
+        try:
+            tottal_amount = input('итоговая стоимость>')
+
+            if not tottal_amount.strip().isdigit():
+                raise ValueError('ошибка>итоговая стоимость должна состоять только из цифр')
+
+            break
+        except ValueError as error:
+            print(error)
+
+    while True:
+        try:
+            balance = input('баланс>')
+
+            if not balance.isdigit():
+                raise ValueError('ошибка>баланс должен состоять только из цифр')
+            elif balance >= tottal_amount:
+                raise ValueError('ошибка>баланс должен быть меньше итоговой суммы')
+
+            break
+        except ValueError as error:
+            print(error)
+
+    current_directory = os.getcwd()
+
+    category_src = f'{current_directory}/categoryes/{category}'
+    if not os.path.exists(f'categoryes/{category}'):
+
+        os.makedirs(category_src)
+
+    goal_src = f'{category_src}/{name}.csv'
+
+    goal = Goal(name, tottal_amount, balance, category)
+
+    with open(goal_src, 'w', encoding='utf-8', newline='') as goal_file:
+        writer = csv.DictWriter(goal_file, ['name', 'tottal_amount', 'balance', 'category', 'status'])
+        writer.writeheader()
+        writer.writerow({'name': name, 'tottal_amount': tottal_amount, 'balance': balance, 'category': category, 'status': goal.get_status()})
+
+    return goal
+
+def delete_goal():
+    while True:
+        try:
+            category = input('название категории>')
+
+            if category.strip() == '':
+                raise ValueError('ошибка>название категории не может состоять только из символов пробела и переноса на новую строку')
+
+            break
+        except ValueError as error:
+            print(error)
+
+    while True:
+        try:
+            name = input('название цели>')
+
+            if name.strip() == '':
+                raise ValueError('ошибка>название цели не может состоять только из символов пробела и переноса на новую строку')
+            
+            break
+        except ValueError as error:
+            print(error)
+
+    try:
+        goal_file_src = f'{os.getcwd()}/categoryes/{category}/{name}.csv'
+
+        os.remove(goal_file_src)
+    except FileNotFoundError:
+        print(f'ошибка>в категории "{category}" нетуцели "{name}"')
+
+def find_goal():
+    while True:
+        try:
+            category = input('название категории>')
+
+            if category.strip() == '':
+                raise ValueError('ошибка>название категории не может состоять только из символов пробела и переноса на новую строку')
+
+            break
+        except ValueError as error:
+            print(error)
+
+    while True:
+        try:
+            name = input('название цели>')
+
+            if name.strip() == '':
+                raise ValueError('ошибка>название цели не может состоять только из символов пробела и переноса на новую строку')
+            
+            break
+        except ValueError as error:
+            print(error)
+
+    goal_file_src = f'{os.getcwd()}/categoryes/{category}/{name}.csv'
+    goal_category_src = f'{os.getcwd()}/categoryes/{category}'
+
+    try:
+        if not os.path.exists(goal_category_src):
+            raise FileNotFoundError(f'сообщение>категория с названием "{category}" не существует')
+        if not os.path.exists(goal_file_src):
+            raise FileNotFoundError(f'сообщение>в категории "{category}" нету цели "{name}"')
+    except FileNotFoundError as error:
+        print(error)
+    
+
+        return
+
+    with open(goal_file_src, 'r', encoding='utf-8') as goal_file:
+        reader = csv.DictReader(goal_file)
+
+        for row in reader:
+            goal = Goal(row.get('name'), int(row.get('tottal_amount')), int(row.get('balance')), row.get('category'))
+
+    command_list = ['увеличить баланс', 'уменьшить баланс', 'информация', 'выход']
+
+    while True:
+        while True:
+            try:
+                command = input(f'Текущая цель: {goal.name}, команда>')
+
+                if not command in command_list:
+                    raise ValueError(f'ошибка>{command}\nсообщение>для просмотра всех допустимых комманд введите "помощь"')
+
+                break
+            except ValueError as error:
+                print(error)
+
+        if command == 'помощь':
+            print(f'''
+                помощь - выводит все доступные команды
+                увеличить баланс - увеличивает баланс цели на введённое значение
+                уменьшить баланс - уменьшает баланс цели на введённое значение
+                выход - завершение работы с текущей целью
+            ''')
+
+        if command == 'выход':
+            return
+
+        if command == 'увеличить баланс':
+            current_balance = goal.balance
+
+            while True:
+                try:
+                    amount = input('сумма>')
+
+                    if not amount.strip().isdigit():
+                        raise ValueError('сумма должна состоять только из цифр')
+
+                    break
+                except ValueError as error:
+                    print(error)
+
+            goal.increase_balance(int(amount))
+
+            if current_balance != goal.balance:
+                with open(goal_file_src, 'r', encoding='utf-8') as goal_file:
+                    reader = csv.DictReader(goal_file)
+                    row = list(reader)[0]
+                    row.update({'balance': goal.balance})
+
+                with open(goal_file_src, 'w', encoding='utf-8', newline='') as goal_file:
+                    writer = csv.DictWriter(goal_file, ['name', 'tottal_amount', 'balance', 'category', 'status'])
+                    writer.writeheader()
+                    writer.writerow(row)
+
+        if command == 'уменьшить баланс':
+            current_balance = goal.balance
+
+            while True:
+                try:
+                    amount = input('сумма>')
+
+                    if not amount.strip().isdigit():
+                        raise ValueError('сумма должна состоять только из цифр')
+
+                    break
+                except ValueError as error:
+                    print(error)
+
+            goal.decrease_balance(int(amount))
+
+            if current_balance != goal.balance:
+                with open(goal_file_src, 'r', encoding='utf-8') as goal_file:
+                    reader = csv.DictReader(goal_file)
+                    row = list(reader)[0]
+                    row.update({'balance': goal.balance})
+
+                with open(goal_file_src, 'w', encoding='utf-8', newline='') as goal_file:
+                    writer = csv.DictWriter(goal_file, ['name', 'tottal_amount', 'balance', 'category', 'status'])
+                    writer.writeheader()
+                    writer.writerow(row)
+
+            if current_balance != goal.balance:
+                with open(goal_file_src, 'r', encoding='utf-8') as goal_file:
+                    reader = csv.DictReader(goal_file)
+                    writer = csv.DictWriter(goal_file, ['name', 'tottal_amount', 'balance', 'category', 'status'])
+                    for row in reader:
+                        row.update({'balance': goal.balance})
+                        writer.writerow(row)
+
+        if command == 'информация':
+            print(f'''
+                название цели - {goal.name}
+                итоговая сумма - {goal.total_amount}
+                текущий баланс - {goal.balance}
+                название категории - {goal.category}
+                статус - {goal.get_status()}
+            ''')
+
+def print_commands():
+    print('''
+    помощь - выводит все доступные команды
+    создать цель - создаёт цель с указанными атрибутами
+    создать категорию - создаёт категорию с указанным названием
+    удалить цель - удаляет цель с указанным названием
+    ''')
+
+def money_box():
+    print('сообщение>Итоговый проект "Копилка"')
+    commands_list = ['помощь', 'выход', 'создать цель', 'удалить цель', 'найти цель']
+    current_goal = None
+
+    while True:
+        while True:
+            try:
+                command = input('команда>')
+
+                if not command in commands_list:
+                    raise ValueError(f'ошибка>неизвестная комманда "{command}"\nсообщение>для просмотра всех допустимых комманд введите "помощь"')
+
+                break
+            except ValueError as error:
+                print(error)
+        if command == 'удалить цель':
+            delete_goal()
+        
+        if command == 'создать цель':
+            current_goal = create_goal()
+
+        if command == 'найти цель':
+            find_goal()
+
+        if command == 'помощь':
+            print_commands()
+        
+        if command == 'выход':
+            break
+
+money_box()
