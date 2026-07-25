531468d4ac52e408a60372ba9df2644e6549ca4f
Kirill Sidora <kirillsidora@yandex.ru>
2026-07-25 18:34:22 +0300

    [MB-2] feat: Created balance methods

--- main.py
+++ main.py
@@ -5,3 +5,21 @@ class Goal:
         self.balance = balance
         self.category = category
         self.status = status
+
+    def increase_balance(self, amount: int):
+        try:
+            if self.balance + amount > self.total_amount:
+                raise ValueError(f'Баланс не может превышать итоговой суммы цели ({self.balance} + {amount} > {self.total_amount})')
+
+            self.balance += amount
+        except ValueError as error:
+            print(error)
+
+    def decrease_balance(self, amount: int):
+        try:
+            if self.balance - amount < 0:
+                raise ValueError(f'Баланс не может быть отрицательным ({self.balance} - {amount} < 0)')
+
+            self.balance -= amount
+        except ValueError as error:
+            print(error)
